e76f2ef59d67e3cd876cc5c0b7aa409efa5e9871
Jopkirs <megaalexs@yahoo.com>
2019-09-03 11:38:30 -0400

    Configure index and userlist pages.

--- src/main/java/guru/springframework/springrestclientexamples/service/ApiServiceImpl.java
+++ src/main/java/guru/springframework/springrestclientexamples/service/ApiServiceImpl.java
@@ -2,9 +2,10 @@ package guru.springframework.springrestclientexamples.service;
 
 import guru.springframework.api.domain.User;
 import guru.springframework.api.domain.UserData;
-import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import java.util.List;
 
@@ -13,14 +14,22 @@ public class ApiServiceImpl implements ApiService {
 
     private RestTemplate restTemplate;
 
-    public ApiServiceImpl(RestTemplate restTemplate) {
+    private final String api_url;
+
+    public ApiServiceImpl(RestTemplate restTemplate, @Value("${api.url}") String api_url) {
         this.restTemplate = restTemplate;
+        this.api_url = api_url;
     }
 
     @Override
     public List<User> getUsers(Integer limit) {
 
-        UserData userData = restTemplate.getForObject("http://private-anon-a4e9684223-apifaketory.apiary-mock.com/api/user?limit=" + limit, UserData.class);
+        String uri;
+        UriComponentsBuilder uriComponentsBuilder = UriComponentsBuilder
+                .fromUriString(api_url)
+                .queryParam("limit", limit);
+
+        UserData userData = restTemplate.getForObject(uriComponentsBuilder.toUriString(), UserData.class);
         return userData.getData();
     }
 }
